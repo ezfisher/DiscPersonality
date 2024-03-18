f4432a7eac409ddf67433ee6e1b6dc2167caa997
Quasar-light <t.d.sande@gmail.com>
2024-03-18 18:01:25 -0400

    add class
    added class personality type

---
+++ utils/class_Personality_type.py
@@ -0,0 +1,9 @@
+# personality type class of all the possible types of personalities a user can generate.
+# the class Personality is an Enum. The attributes Personality.DOMINANCE, Personality.INFLUENCE, etc., are enumeration members (or members) and are functionally constants.
+#The enum members have names and values (the name of Personality.DOMINANCE is DOMINANCE, the value of Personality.INFLUENCE is 2, etc.)
+
+class PersonalityType(Enum):
+    DOMINANCE = 1
+    INFLUENCE = 2
+    STEADY = 3
+    CONSCIENTIOUS = 4
